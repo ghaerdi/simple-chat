f6d473a6108f69dd514f4fc7d871e33d7895893f
ghaerdi <ghaerdi@outlook.com>
2022-03-28 00:23:57 -0400

    chore: remove unused import

--- frontend/src/components/ChatRoom.tsx
+++ frontend/src/components/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import Message from "./UI/message";
 import useMessage from "../customHooks/useMessage";
 import ws from "../chat";
